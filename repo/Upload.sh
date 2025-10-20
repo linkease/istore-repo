b00d4e613982b4f16db27763082441f4ef293727
Liangbin Lian <1129525450@qq.com>
2025-10-20 17:23:38 +0800

    ci: show progress on upload ipks
    Added progress option to rsync commands for better visibility.

--- repo/Upload.sh
+++ repo/Upload.sh
@@ -1,9 +1,9 @@
 
 for arch in `ls bin/packages | grep -v '^\.' | grep -v '^all$'`; do
-    rsync --exclude='Packages' --exclude='Packages.*' -a bin/packages/$arch $REPO_DEST/
+    rsync --progress --exclude='Packages' --exclude='Packages.*' -a bin/packages/$arch $REPO_DEST/
     rsync -a --del bin/packages/$arch/nas $REPO_DEST/$arch/
 done
-rsync --exclude='Packages' --exclude='Packages.*' -a bin/packages/all/nas_luci $REPO_DEST/all/
+rsync --progress --exclude='Packages' --exclude='Packages.*' -a bin/packages/all/nas_luci $REPO_DEST/all/
 rsync -a --del bin/packages/all/nas_luci $REPO_DEST/all/
 
 for conf in `find -H bin/packages/all -maxdepth 1 -type f -name '*.conf'`; do
