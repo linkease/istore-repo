c80c024013e7a1a3f04f87757f64a32df96f845e
jjm2473 <jjm2473@gmail.com>
2026-06-16 21:24:39 +0800

    ci: index and upload istore

--- repo/Index.sh
+++ repo/Index.sh
@@ -3,10 +3,12 @@ for arch in `ls bin/packages | grep -v '^\.' | grep -v '^all$'`; do
     [ -d bin/packages/$arch ] || continue
     INDEX_ARCH=$arch INDEX_FEEDS=nas make -f repo/Index.mk package/index || exit 1
 done
+INDEX_ARCH=all INDEX_FEEDS=store make -f repo/Index.mk package/index || exit 1
 INDEX_ARCH=all INDEX_FEEDS=nas_luci make -f repo/Index.mk package/index || exit 1
 
 for arch in `ls bin/apks | grep -v '^\.' | grep -v '^all$'`; do
     [ -d bin/apks/$arch ] || continue
     INDEX_ARCH=$arch INDEX_FEEDS=nas make -f repo/Index.mk package/mkndx || exit 1
 done
+INDEX_ARCH=all INDEX_FEEDS=store make -f repo/Index.mk package/mkndx || exit 1
 INDEX_ARCH=all INDEX_FEEDS=nas_luci make -f repo/Index.mk package/mkndx || exit 1
