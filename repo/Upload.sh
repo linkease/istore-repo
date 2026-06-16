1578ffae3398e021fbe5d8b9b945f13c9eaa9cb6
jjm2473 <jjm2473@gmail.com>
2026-06-16 21:24:39 +0800

    ci: index and upload istore

--- repo/Upload.sh
+++ repo/Upload.sh
@@ -5,12 +5,9 @@ for arch in `ls bin/packages | grep -v '^\.' | grep -v '^all$'`; do
     rsync --progress --exclude='Packages' --exclude='Packages.*' -a bin/packages/$arch $REPO_DEST/
     rsync -a --del bin/packages/$arch/nas $REPO_DEST/$arch/
 done
-rsync --progress --exclude='Packages' --exclude='Packages.*' -a bin/packages/all/nas_luci $REPO_DEST/all/
+rsync --progress --exclude='Packages' --exclude='Packages.*' -a bin/packages/all $REPO_DEST/
+rsync -a --del bin/packages/all/store $REPO_DEST/all/
 rsync -a --del bin/packages/all/nas_luci $REPO_DEST/all/
-
-for conf in `find -H bin/packages/all -maxdepth 1 -type f -name '*.conf'`; do
-    rsync -a $conf $REPO_DEST/all/
-done
 
 # APKs
 for arch in `ls bin/apks | grep -v '^\.' | grep -v '^all$'`; do
@@ -19,6 +16,7 @@ for arch in `ls bin/apks | grep -v '^\.' | grep -v '^all$'`; do
     rsync --include="*/" --include='packages.adb' --exclude="*" -a bin/apks/$arch $REPO_DEST-apk/
     rsync -a --del bin/apks/$arch/nas $REPO_DEST-apk/$arch/
 done
-rsync --progress --exclude='packages.adb' -a bin/apks/all/nas_luci $REPO_DEST-apk/all/
-rsync --include="*/" --include='packages.adb' --exclude="*" -a bin/apks/all/nas_luci $REPO_DEST-apk/all/
+rsync --progress --exclude='packages.adb' -a bin/apks/all $REPO_DEST-apk/
+rsync --include="*/" --include='packages.adb' --exclude="*" -a bin/apks/all $REPO_DEST-apk/
+rsync -a --del bin/apks/all/store $REPO_DEST-apk/all/
 rsync -a --del bin/apks/all/nas_luci $REPO_DEST-apk/all/
